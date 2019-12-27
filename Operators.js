ae92a239fe55c7f77030abdc23481c22a718d59a
NasirAhamedB <56265386+NasirAhamedB@users.noreply.github.com>
2019-12-27 17:43:03 -0500

    Add files via upload

---
+++ Operators.js
@@ -0,0 +1,33 @@
+
+//JS Arithimetic Operators:
+// +                 Addition
+// -                 Subtraction
+// *                 Multiplication
+// **                Exponentiation
+// /                 Division
+// %                 Modulus
+// ++                Increment
+// --                Decrement
+
+var a = 100;
+var b = 50;
+console.log(a + b);
+console.log(a - b)
+console.log(a * b)
+console.log(a ** b)
+console.log(a / b)
+console.log(a % b)
+console.log(a++); // Not working 
+console.log(b--); // Not working 
+
+
+// JS Assignment Operators
+//    Assignment operators assign values to JavaScript variables.
+// =        x = y         x = y
+// +=       x += y        x = x+y
+// -=       x -= y        x = x-y
+// *=       x *= y        x = x*y
+// /=
+// %=
+// **=
+
